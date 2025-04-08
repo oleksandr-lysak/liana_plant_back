b77b9ab875705543508403437ddbf2cb4832fe62
Oleksand Lysak <lysak.olexandr@gmail.com>
2025-04-08 19:26:42 +0200

    feat: update styles for ApplicationLogo, MasterCard, and NavLink components

--- resources/js/Components/MasterCard.vue
+++ resources/js/Components/MasterCard.vue
@@ -2,18 +2,17 @@
   <div
     class="flex flex-col sm:flex-row justify-between sm:items-center dark:bg-gray-800 bg-white shadow-md mt-2 p-4 hover:shadow-xl rounded-2xl cursor-pointer transition-all duration-300">
     
-    <!-- Ліва частина: Фото та інформація -->
     <div class="flex flex-col sm:flex-row items-start">
       <img
         :src="imageUrl || 'https://i.imgur.com/aq39RMA.jpeg'"
-        class="w-24 h-24 sm:w-28 sm:h-28 rounded-lg object-cover border-2 border-gray-400 shadow-sm"
+        class="w-24 h-24 sm:w-28 sm:h-28 rounded-lg object-cover border-2 dark:border-green-400 border-blue-500 shadow-sm"
       />
       <div class="flex flex-col ml-0 sm:ml-4 mt-2 sm:mt-0 space-y-1">
         <span class="font-semibold text-lg dark:text-gray-100 text-gray-800">{{ name }}</span>
         <span class="text-sm dark:text-gray-300 text-gray-500 truncate max-w-xs sm:max-w-sm md:max-w-md lg:max-w-lg">
           {{ description }}
         </span>
-        <div class="flex items-center flex-wrap gap-x-2 text-sm dark:text-gray-400 text-gray-600">
+        <div class="flex items-center flex-wrap gap-x-2 text-sm dark:text-green-400 text-blue-500">
           <span>Age: {{ age }}</span>
           <span>|</span>
           <span>Experience: 5 years</span>
@@ -32,11 +31,10 @@
       </div>
     </div>
 
-    <!-- Права частина: Час, рейтинг -->
     <div class="flex flex-col sm:items-end items-start mt-4 sm:mt-0 space-y-1">
       <span class="text-sm dark:text-gray-300 text-gray-500">2 reviews</span>
       <div class="flex items-center space-x-1">
-        <i class="fa fa-star text-yellow-400"></i>
+        <i class="fa fa-star dark:text-green-400 text-blue-500"></i>
         <span class="text-sm font-medium dark:text-gray-400 text-gray-600">{{ rating }}</span>
       </div>
     </div>
