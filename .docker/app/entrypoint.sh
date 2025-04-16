5fe75560797e2c34509575fad231fa5a4b8520ef
Oleksand Lysak <lysak.olexandr@gmail.com>
2025-04-16 20:54:23 +0200

    fix: remove Telegram notification logic from entrypoint script

--- .docker/app/entrypoint.sh
+++ .docker/app/entrypoint.sh
@@ -1,16 +1,5 @@
 #!/bin/sh
-set -euo pipefail
-trap 'echo "Error on line $LINENO"' ERR
-
-if [ ! -z "$TELEGRAM_TOKEN" ] && [ ! -z "$TELEGRAM_CHAT_ID" ]; then
-    MESSAGE="✅ Deploy has been start <b>$(hostname)</b> enviroment <code>$APP_ENV</code> 🚀"
-    curl -s -X POST "https://api.telegram.org/bot$TELEGRAM_TOKEN/sendMessage" \
-        -d chat_id="$TELEGRAM_CHAT_ID" \
-        -d text="$MESSAGE" \
-        -d parse_mode="HTML"
-fi
-
-
+set -e
 
 git config --global --add safe.directory /var/www/html
 chmod -R 775 /var/www/html/storage /var/www/html/bootstrap/cache || true
@@ -27,13 +16,5 @@ php artisan view:cache
 npm install
 npm run build
 php artisan migrate
-
-if [ ! -z "$TELEGRAM_TOKEN" ] && [ ! -z "$TELEGRAM_CHAT_ID" ]; then
-    MESSAGE="✅ Deploy success on <b>$(hostname)</b> with env <code>$APP_ENV</code> 🚀"
-    curl -s -X POST "https://api.telegram.org/bot$TELEGRAM_TOKEN/sendMessage" \
-        -d chat_id="$TELEGRAM_CHAT_ID" \
-        -d text="$MESSAGE" \
-        -d parse_mode="HTML"
-fi
 
 exec supervisord -c /etc/supervisor/supervisord.conf
