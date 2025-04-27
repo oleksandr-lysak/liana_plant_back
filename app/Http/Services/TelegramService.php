efe7b3afd4d860adda11fe89abf04fd534029a5b
Oleksand Lysak <lysak.olexandr@gmail.com>
2025-04-27 23:02:20 +0200

    feat: implement message chunking and error reporting in TelegramService

--- app/Http/Services/TelegramService.php
+++ app/Http/Services/TelegramService.php
@@ -5,28 +5,70 @@ namespace App\Http\Services;
 use JsonException;
 use NotificationChannels\Telegram\Exceptions\CouldNotSendNotification;
 use NotificationChannels\Telegram\TelegramMessage;
+use Throwable;
 
 /**
  * Class TelegramService
  *
- * This service class provides functionality to send messages to a Telegram chat using the Telegram Bot API.
+ * Service for sending messages to a Telegram chat using the Telegram Bot API.
  */
 class TelegramService
 {
+    private const int MAX_TELEGRAM_MESSAGE_LENGTH = 4096;
+
     /**
-     * Sends a message to a specified Telegram chat.
+     * Sends a message to a Telegram chat.
      *
-     * @param  string  $message  The message content to be sent.
+     * @param string $message The message content to send.
      *
-     * @throws CouldNotSendNotification If the notification could not be sent.
-     * @throws JsonException If there is an error encoding the message to JSON.
+     * @throws CouldNotSendNotification
+     * @throws JsonException
      */
-    public static function sendTelegramMessage(string $message): void
+    public function send(string $message): void
+    {
+        $chunks = str_split($message, self::MAX_TELEGRAM_MESSAGE_LENGTH);
+
+        foreach ($chunks as $chunk) {
+            $this->sendChunk($chunk);
+        }
+    }
+
+    /**
+     * Reports an exception to the Telegram chat.
+     *
+     * @param Throwable $exception The exception to report.
+     *
+     * @throws CouldNotSendNotification
+     * @throws JsonException
+     */
+    public function report(Throwable $exception): void
+    {
+        $message = sprintf(
+            "<b>Error on server (%s)</b>\n\n<b>Message:</b> %s\n<b>File:</b> %s:%d\n\n<code>%s</code>",
+            config('app.env'),
+            $exception->getMessage(),
+            $exception->getFile(),
+            $exception->getLine(),
+            $exception->getTraceAsString()
+        );
+
+        $this->send($message);
+    }
+
+    /**
+     * Sends a single message chunk to Telegram.
+     *
+     * @param string $chunk The message chunk.
+     *
+     * @throws CouldNotSendNotification
+     * @throws JsonException
+     */
+    private function sendChunk(string $chunk): void
     {
         TelegramMessage::create()
             ->token(config('services.telegram-bot-api.token'))
             ->to(config('services.telegram-bot-api.chat_id'))
-            ->content($message)
+            ->content($chunk)
             ->send();
     }
 }
