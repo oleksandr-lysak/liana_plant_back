49843cb8d9f715d6060546c5e7420dc90215084e
Oleksand Lysak <lysak.olexandr@gmail.com>
2025-04-27 23:02:04 +0200

    feat: add command to sync appointment data with Redis

---
+++ app/Console/Commands/SyncAppointmentsRedis.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Http\Services\Appointment\AppointmentRedisService;
+use App\Models\Appointment;
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Redis;
+
+class SyncAppointmentsRedis extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'app:sync-appointments-redis';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    protected AppointmentRedisService $appointmentRedisService;
+
+    public function __construct(AppointmentRedisService $appointmentRedisService)
+    {
+        parent::__construct();
+        $this->appointmentRedisService = $appointmentRedisService;
+    }
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        $this->info('Починаємо синхронізацію даних про зайнятість з бази даних у Redis...');
+
+        // Отримуємо всі активні записи про призначення з бази даних
+        $appointments = Appointment::all(); // Або використовуйте більш точний запит, якщо потрібно
+
+        // Очищаємо всі існуючі дані про зайнятість у Redis (за потреби)
+        // Можна розглянути очищення лише ключів, пов'язаних із зайнятістю
+        // Redis::flushdb();
+        $masters = Appointment::distinct('master_id')->pluck('master_id');
+        foreach ($masters as $masterId) {
+            Redis::del($this->appointmentRedisService->getMasterBusyIntervalsKey($masterId));
+        }
+
+        // Заповнюємо Redis даними з бази даних
+        foreach ($appointments as $appointment) {
+            $this->appointmentRedisService->markAsBusy(
+                $appointment->master_id,
+                $appointment->start_time,
+                $appointment->end_time
+            );
+        }
+
+        $this->info('Синхронізацію даних про зайнятість завершено.');
+    }
+}
