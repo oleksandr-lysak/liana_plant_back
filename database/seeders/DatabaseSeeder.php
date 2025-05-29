2972b22225a15878838fda4c22c704ae33cd5d27
Oleksand Lysak <lysak.olexandr@gmail.com>
2025-05-29 17:31:59 +0200

    refactor: comment out master, review, and client creation logic in DatabaseSeeder for performance optimization

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -46,34 +46,34 @@ class DatabaseSeeder extends Seeder
             Service::create(['name' => $serviceName]);
         }
         // count of masters
-        $total = 1000;
-        $this->command->info("Creating {$total} masters...");
-        $this->command->getOutput()->progressStart($total);
-        for ($i = 0; $i < $total; $i++) {
-            Master::factory()->create();
-            $this->command->getOutput()->progressAdvance();
-        }
-        $this->command->getOutput()->progressFinish();
+        // $total = 1000;
+        // $this->command->info("Creating {$total} masters...");
+        // $this->command->getOutput()->progressStart($total);
+        // for ($i = 0; $i < $total; $i++) {
+        //     Master::factory()->create();
+        //     $this->command->getOutput()->progressAdvance();
+        // }
+        // $this->command->getOutput()->progressFinish();
 
-        // count of reviews
-        $total = 10000;
-        $this->command->info("Creating {$total} reviews...");
-        $this->command->getOutput()->progressStart($total);
-        for ($i = 0; $i < $total; $i++) {
-            Review::factory()->create();
-            $this->command->getOutput()->progressAdvance();
-        }
-        $this->command->getOutput()->progressFinish();
+        // // count of reviews
+        // $total = 10000;
+        // $this->command->info("Creating {$total} reviews...");
+        // $this->command->getOutput()->progressStart($total);
+        // for ($i = 0; $i < $total; $i++) {
+        //     Review::factory()->create();
+        //     $this->command->getOutput()->progressAdvance();
+        // }
+        // $this->command->getOutput()->progressFinish();
 
-        // count of clients
-        $total = 10000;
-        $this->command->info("Creating {$total} clients...");
-        $this->command->getOutput()->progressStart($total);
-        for ($i = 0; $i < $total; $i++) {
-            Client::factory()->create();
-            $this->command->getOutput()->progressAdvance();
-        }
-        $this->command->getOutput()->progressFinish();
+        // // count of clients
+        // $total = 10000;
+        // $this->command->info("Creating {$total} clients...");
+        // $this->command->getOutput()->progressStart($total);
+        // for ($i = 0; $i < $total; $i++) {
+        //     Client::factory()->create();
+        //     $this->command->getOutput()->progressAdvance();
+        // }
+        // $this->command->getOutput()->progressFinish();
 
     }
 }
--- docker/entrypoint.prod.sh
+++ docker/entrypoint.prod.sh
@@ -6,5 +6,3 @@ php artisan cache:clear
 php artisan config:clear
 php artisan route:clear
 php artisan view:clear
-
-exec /opt/docker/bin/entrypoint supervisord 
